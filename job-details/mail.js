69f21427fc52a29ed224147dbc2c4ca84f747e0c
Dinesh Stark <111217823+DineshStark7@users.noreply.github.com>
2025-03-01 22:42:20 +0530

    Updated images in life-at-kaar

--- job-details/mail.js
+++ job-details/mail.js
@@ -1,16 +1,16 @@
-document.getElementById("applyButton").addEventListener("click", function () {
-  const jobTitle = document.getElementById("job-title");
-  console.log(jobTitle.innerText);
-  const email = "hr@kaarinfotech.com";
-  const subject = "Job Application - [Mention your Role]"; // Optional, can be changed
-  const body =
-    "Dear HR,\n\nI am interested in applying for the position. Here's my latest resume \n\n [Attach you latest Resume]\n\n\n [Remove the square brackets and you can change template if you have one.]"; // Optional
+const applyBtn = () => {
+  document.getElementById("applyButton").addEventListener("click", function () {
+    const jobTitle = document.getElementById("job-title").innerText;
+    const email = "hr@kaarinfotech.com";
+    const subject = `Job Application - ${jobTitle}`; // Optional, can be changed
+    const body = `Dear HR,\n\nI am interested in applying for the position "${jobTitle}". Here's my latest resume \n\n [Attach you latest Resume]\n\n\n [Remove the square brackets and you can change template if you have one.]`; // Optional
 
-  // Construct the mailto link
-  const mailtoLink = `mailto:${email}?subject=${encodeURIComponent(
-    subject
-  )}&body=${encodeURIComponent(body)}`;
+    // Construct the mailto link
+    const mailtoLink = `mailto:${email}?subject=${encodeURIComponent(
+      subject
+    )}&body=${encodeURIComponent(body)}`;
 
-  // Open the default mail client
-  window.location.href = mailtoLink;
-});
+    // Open the default mail client
+    window.location.href = mailtoLink;
+  });
+};
